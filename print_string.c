910226e0a25e0c8a7641499e71de7654d42accfd
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-03-11 18:51:21 -0800

    problem0

--- print_string.c
+++ print_string.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 /*
- *
- *
+ * print_string - writes the character
+ * Description: We are printing a string without using printf
+ * @s: string to be printed
+ * Return: 1
  */
 int print_string(va_list s)
 {
@@ -14,7 +16,7 @@ int print_string(va_list s)
 
 	if (string == NULL)
 		string = "(null)";
-	
+
 	while (string[i])
 	{
 		_putchar(string[i]);
